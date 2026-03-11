92f3ee48822ff7ecdda86e181f168154d9ed0213
Tynahan <nusilite@gmail.com>
2026-03-11 21:17:37 +0900

    change keymaps

--- keymaps/carrot/keymap.c
+++ keymaps/carrot/keymap.c
@@ -71,18 +71,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // New Layer (HD Promethium)
 [_NEW] = LAYOUT_ortho_4x12(
-    KC_Q,         KC_F,    KC_P,  KC_D,          KC_L,     KC_X,           KC_SEMICOLON, KC_U,   KC_O,             KC_Y,      KC_B,            KC_Z,
-    KC_TAB,       KC_S,    KC_N,  KC_T,          KC_H,     KC_K,           KC_BACKSPACE, KC_A,   KC_E,             KC_I,      KC_C,            KC_ENTER,
-    KC_BACKSLASH, KC_V,    KC_W,  KC_G,          KC_M,     KC_J,           KC_MINUS,     KC_DOT, KC_QUOTE,         KC_EQUAL,  KC_SLASH,        KC_F13,
-    KC_F13,       KC_DOWN, KC_UP, KC_LEFT_SHIFT, KC_SPACE, LT(_NEWSPECIAL, KC_ESC),      KC_R,   MT(MOD_LSFT, KC_COMMA), MO(_NEWSPECIAL), KC_LEFT, KC_RIGHT, TO(_BASE)
+    KC_Q,         KC_F,    KC_P,  KC_D,          KC_L,     KC_X,           KC_SEMICOLON, KC_U,   KC_O,            KC_Y,      KC_B,     KC_Z,
+    KC_TAB,       KC_S,    KC_N,  KC_T,          KC_H,     KC_K,           KC_BACKSPACE, KC_A,   KC_E,            KC_I,      KC_C,     KC_ENTER,
+    KC_BACKSLASH, KC_V,    KC_W,  KC_G,          KC_M,     KC_J,           KC_MINUS,     KC_DOT, KC_DOUBLE_QUOTE, KC_EQUAL,  KC_SLASH, KC_GRAVE,
+    KC_F13,       KC_DOWN, KC_UP, KC_LEFT_SHIFT, KC_SPACE, LT(_NEWSPECIAL, KC_ESC),      KC_R,   MT(MOD_LSFT,     KC_COMMA), KC_QUOTE, KC_LEFT, KC_RIGHT, TO(_BASE)
 ),
 
 // NewTop Layer (Numbers & Brackets)
 [_NEWSPECIAL] = LAYOUT_ortho_4x12(
-    _______, KC_0,    KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    _______,
-    _______, _______, _______, KC_GRAVE, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, _______, _______, _______, _______,
-    _______, _______, _______, _______,  KC_LABK, KC_LBRC, KC_RBRC, KC_RABK, _______, _______, _______, _______,
-    _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______
+    _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    _______,
+    _______, _______, _______, KC_LPRN, KC_LCBR, _______, _______, KC_RCBR, KC_RPRN, _______, _______, _______,
+    _______, _______, _______, KC_LABK, KC_LBRC, _______, _______, KC_RBRC, KC_RABK, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 
