dd9fbbcc8116187c7833443f300ca43567b52c57
Tynahan <nusilite@gmail.com>
2026-03-19 23:16:53 +0900

    feat: change cmd key location

--- CHEATSHEET.md
+++ CHEATSHEET.md
@@ -8,7 +8,3 @@ qmk flash -kb keebio/nyquist/rev5 -km carrot
 ```
 
 2. set the keyboard bootloader mode
-
-- press reset button twice this is too much in I thini I Jonathan Blhhtow
-  sldkfjsdlkffhhnthntihenthientehntiehhntiehnthahaththhahaitf
-  b....====""""".....==="""-----///////,,,,,,,,012301230123!@##$e,!)1))))!!<P<P!)!!!<P''''''''''''''''''''''''""""""""hahahahahahahahahahahahahaha
--- keymaps/carrot/keymap.c
+++ keymaps/carrot/keymap.c
@@ -11,10 +11,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //  Base Layer (QWERTY)
 [_BASE] = LAYOUT_ortho_4x12(
-    KC_F13,       KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,   KC_DEL,
-    KC_TAB,       KC_A,    KC_S,     KC_D,    KC_F,   KC_G,   KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L,   KC_ENT,
-    KC_LSFT,      KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT, KC_RSFT,
-    KC_CAPS_LOCK, KC_LEFT, KC_RIGHT, KC_LALT, KC_SPC, KC_ESC, MO(_RAISE), KC_RSFT, KC_RCTL, KC_DOWN, KC_UP,  KC_RGUI
+    KC_F13,   KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,   KC_DEL,
+    KC_TAB,   KC_A,    KC_S,     KC_D,    KC_F,   KC_G,   KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L,   KC_ENT,
+    KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT, KC_RSFT,
+    KC_QUOTE, KC_LEFT, KC_RIGHT, KC_LCTL, KC_SPC, KC_ESC, MO(_RAISE), KC_RSFT, KC_LALT, KC_DOWN, KC_UP,  KC_RGUI
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12(
