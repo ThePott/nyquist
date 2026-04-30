2f0d0059fb9388a3eef95a171053ba65726f2ef0
Tynahan <nusilite@gmail.com>
2026-04-30 21:38:44 +0900

    feat: roll back equal and double quote

--- keymaps/carrot/keymap.c
+++ keymaps/carrot/keymap.c
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12(
-    KC_GRV,  KC_0,         KC_1,         KC_2,     KC_3,            KC_4,          KC_5,    KC_6,     KC_7,     KC_8,    KC_9,    _______,
-    KC_TILD, KC_LBRC,      KC_LABK,      KC_LPRN,  KC_LCBR,         KC_MINUS,      _______, KC_RCBR,  KC_RPRN,  KC_RABK, KC_RBRC, _______,
-    _______, KC_SEMICOLON, KC_BACKSLASH, KC_QUOTE, KC_DOUBLE_QUOTE, KC_UNDERSCORE, KC_EQL,  KC_COLON, KC_SLASH, _______, _______, _______,
-    _______, _______,      _______,      _______,  _______,         _______,       _______, _______,  _______,  _______, _______, _______
+    KC_GRV,  KC_0,         KC_1,         KC_2,     KC_3,    KC_4,          KC_5,            KC_6,     KC_7,     KC_8,    KC_9,    _______,
+    KC_TILD, KC_LBRC,      KC_LABK,      KC_LPRN,  KC_LCBR, KC_MINUS,      _______,         KC_RCBR,  KC_RPRN,  KC_RABK, KC_RBRC, _______,
+    _______, KC_SEMICOLON, KC_BACKSLASH, KC_QUOTE, KC_EQL,  KC_UNDERSCORE, KC_DOUBLE_QUOTE, KC_COLON, KC_SLASH, _______, _______, _______,
+    _______, _______,      _______,      _______,  _______, _______,       _______,         _______,  _______,  _______, _______, _______
 ),
 
 // NewFunction Layouer (Functions for accessing bios)
