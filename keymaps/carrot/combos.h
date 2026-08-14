c17b5c9c85379ab974587ac56e31ff4659e2f1f1
Tynahan <nusilite@gmail.com>
2026-08-14 10:20:46 +0900

    change ., order

--- keymaps/carrot/combos.h
+++ keymaps/carrot/combos.h
@@ -1,14 +1,17 @@
 #pragma once
 #include "keycodes.h"
+#include "keymap_us.h"
 #include "process_combo.h"
 #include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 
 // NOTE: HD Pothenium combo
 const uint16_t PROGMEM combo_lhypr[] = {KC_Z, KC_S, COMBO_END};
-const uint16_t PROGMEM combo_rhypr[] = {KC_DOT, KC_K, COMBO_END};
+const uint16_t PROGMEM combo_rhypr[] = {KC_COMM, KC_K, COMBO_END};
+// const uint16_t PROGMEM combo_underscore[] = {KC_SPACE, KC_RIGHT_SHIFT, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_lhypr, KC_HYPR),
     COMBO(combo_rhypr, KC_HYPR),
+    // COMBO(combo_underscore, KC_UNDERSCORE),
 };
--- keymaps/carrot/keymap.c
+++ keymaps/carrot/keymap.c
@@ -11,17 +11,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 //  Base Layer (QWERTY)
 [_BASE] = LAYOUT_ortho_4x12(
-    KC_F13,   KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,   KC_DEL,
-    KC_TAB,   KC_A,    KC_S,     KC_D,    KC_F,   KC_G,   KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L,   KC_ENT,
-    KC_LSFT,  KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT, KC_RSFT,
-    MO(_FUNCTION), KC_LEFT, KC_RIGHT, KC_LCTL, KC_SPC, KC_ESC, MO(_RAISE), KC_RSFT, KC_LALT, KC_DOWN, KC_UP,  KC_RGUI
+    KC_F13,        KC_Q,    KC_W,     KC_E,    KC_R,   KC_T,   KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+    KC_TAB,        KC_A,    KC_S,     KC_D,    KC_F,   KC_G,   KC_BSPC,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+    KC_LSFT,       KC_Z,    KC_X,     KC_C,    KC_V,   KC_B,   KC_B,       KC_N,    KC_M,    KC_DOT,  KC_COMM, KC_RSFT,
+    MO(_FUNCTION), KC_LEFT, KC_RIGHT, KC_LCTL, KC_SPC, KC_ESC, MO(_RAISE), KC_RSFT, KC_LALT, KC_DOWN, KC_UP,   KC_RGUI
 ),
 
 [_RAISE] = LAYOUT_ortho_4x12(
-    KC_GRV,  KC_0,         KC_1,         KC_2,     KC_3,    KC_4,          KC_5,            KC_6,     KC_7,     KC_8,    KC_9,    _______,
-    KC_TILD, KC_LBRC,      KC_LABK,      KC_LPRN,  KC_LCBR, KC_MINUS,      _______,         KC_RCBR,  KC_RPRN,  KC_RABK, KC_RBRC, _______,
-    _______, KC_SEMICOLON, KC_BACKSLASH, KC_QUOTE, KC_EQL,  KC_UNDERSCORE, KC_DOUBLE_QUOTE, KC_COLON, KC_SLASH, _______, _______, _______,
-    _______, _______,      _______,      _______,  _______, _______,       _______,         _______,  _______,  _______, _______, _______
+    KC_GRV,  KC_0,         KC_1,     KC_2,     KC_3,         KC_4,          KC_5,            KC_6,     KC_7,    KC_8,    KC_9,    _______,
+    KC_TILD, KC_LBRC,      KC_LABK,  KC_LCBR,  KC_LPRN,      KC_MINUS,      _______,         KC_RPRN,  KC_RCBR, KC_RABK, KC_RBRC, _______,
+    _______, KC_BACKSLASH, KC_SLASH, KC_QUOTE, KC_SEMICOLON, KC_UNDERSCORE, KC_DOUBLE_QUOTE, KC_COLON, KC_EQL,  _______, _______, _______,
+    _______, _______,      _______,  _______,  _______,      _______,       _______,         _______,  _______, _______, _______, _______
 ),
 
 // NewFunction Layouer (Functions for accessing bios)
